0b354a9af45f199a43d5bcb5dd796829576690a2
IceCold4701 <SLydiaR.IceCold@gmail.com>
2024-11-19 19:29:05 +0530

    sds

---
+++ pygame window.py
@@ -0,0 +1,8 @@
+#import necessary libraries 
+from tkinter import *
+#create window
+window = Tk()
+#set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400x300')
+window.mainloop()
